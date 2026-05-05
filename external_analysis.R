b148f2df343caaf443fdfb3b70fae49ffe3bc7ae
Gregoery J. Matthews <gregorymatthews@Gregorys-MacBook-Pro.local>
2026-05-05 17:10:54 -0500

    updated external analysis.

--- external_analysis.R
+++ external_analysis.R
@@ -2,7 +2,6 @@
 #   population + wealth + 
 #   Imports + Exports + foreign aid + (Coldwar / post cold war?)
 # (1|Africa Country) + (1|Year) + (1|Country?)
-
 ## Load in packages and data sets ##
 library(tidyverse)
 library(readxl)
@@ -19,7 +18,7 @@ masterset = read_csv("data/Master 08-28-2025 AFP excel dataset-3.csv")
 # out <- masterset %>% select(COUNTRY, YEAR, POLITY, POLITY2) %>% filter(POLITY != POLITY2) 
 # write.csv(out, file = "/Users/gregorymatthews/polity_vs_polity2.csv",row.names = TRUE)
 
-diplomaticrep = read_csv("data/Master 08-28-2025 AFP excel dataset-3_diplomatic_represetations.csv")
+#diplomaticrep = read_csv("data/Master 08-28-2025 AFP excel dataset-3_diplomatic_represetations.csv")
 
 restofworld = read_excel("data/Rest of the World.xlsx")
 
@@ -34,17 +33,32 @@ masterset = masterset %>%
                              COUNTRY == "sao tome & Principe" ~ "Sao Tome & Principe",
                              .default = COUNTRY))
 
+#Convert commas to periods. 
+#it looks like commas are being used as the decimal.  
+#Fix the nonsense formatting from excel. 
+masterset$GNI <- gsub(",",".",masterset$GNI)
+masterset$GNI <- gsub(" ","",masterset$GNI)%>% as.numeric()
 
-#Manuallty fixing names
-names(masterset)[which(names(masterset) == "TRDX352b")] <- "TRDEX352b"
+masterset$GNI_CAP <- gsub(",",".",masterset$GNI_CAP)
+masterset$GNI_CAP <- gsub(" ","",masterset$GNI_CAP)%>% as.numeric()
+
+masterset$POPULATN <- gsub(",",".",masterset$POPULATN)
+masterset$POPULATN <- gsub(" ","",masterset$POPULATN) %>% as.numeric()
+
+##############
+#Add lags!!!
+##############
+
+#Manually fixing names
+names(masterset)[which(names(masterset) == "TRDX352b")] <- "TRDEX352b" #These years are getting dropped.  
 names(masterset)[which(names(masterset) == "TRDX200h")] <- "TRDEX200h"
 names(masterset)[which(names(masterset) == "TRDX200m")] <- "TRDEX200m"
 names(masterset)[which(names(masterset) == "TDX321sr")] <- "TRDEX321sr"
 names(masterset)[which(names(masterset) == "TDX321sm")] <- "TRDEX321sm"
 names(masterset)[which(names(masterset) == "TDX321yr")] <- "TRDEX321yr"
+
 names(masterset)[which(names(masterset) == "TRDI200h")] <- "TRDIM200h"
 names(masterset)[which(names(masterset) == "TRDI200m")] <- "TRDIM200m"
-
 names(masterset)[which(names(masterset) == "TDI321sr")] <- "TRDIM321sr"
 names(masterset)[which(names(masterset) == "TDI321sm")] <- "TRDIM321sm"
 names(masterset)[which(names(masterset) == "TDI321yr")] <- "TRDIM321yr"
@@ -64,10 +78,22 @@ restofworld <- restofworld %>%
                              COUNTRY == "sao tome & Principe" ~ "Sao Tome & Principe",
                              .default = COUNTRY))
 
+#Convert commas to periods. 
+#it looks like commas are being used as the decimal.  
+#Fix the nonsense formatting from excel. 
+restofworld$GNI <- gsub(",",".",restofworld$GNI)
+restofworld$GNI <- gsub(" ","",restofworld$GNI)%>% as.numeric()
+
+restofworld$GNI_CAP <- gsub(",",".",restofworld$GNI_CAP)
+restofworld$GNI_CAP <- gsub(" ","",restofworld$GNI_CAP)%>% as.numeric()
+
+restofworld$POPULATN <- gsub(",",".",restofworld$POPULATN)
+restofworld$POPULATN <- gsub(" ","",restofworld$POPULATN) %>% as.numeric()
+
+
 ## WORK INVOLVING NOTES FROM 3-27-26 ##
 
 library(data.table)
-
 #Get all the countries
 countrycodes <- list(
   world = restofworld %>%
@@ -85,10 +111,10 @@ countrycodes <- list(
 #external democracy, GNI, GNI_CAP, POPULATN
 external_democracy <- list(
   world = restofworld %>%
-    select(CCODE,YEAR,TOTLIB1, POLITY2,GNI, GNI_CAP, POPULATN) %>%
+    select(CCODE,YEAR,TOTLIB1, POLITY, POLITY2,GNI, GNI_CAP, POPULATN) %>%
     unique(),
   africa = masterset %>%
-    select(CCODE,YEAR,TOTLIB1, POLITY2,GNI, GNI_CAP, POPULATN) %>%
+    select(CCODE,YEAR,TOTLIB1, POLITY, POLITY2,GNI, GNI_CAP, POPULATN) %>%
     mutate(CCODE = paste0("C", CCODE)) %>%
     unique()
 ) %>%
@@ -113,7 +139,8 @@ newdata <- masterset %>%
          everything()) %>%
   filter(CONNECTION %in% c(0, 1), CCODE_INT != CCODE_EXT) %>% 
   left_join(external_democracy, by = c("CCODE_EXT" = "CCODE","YEAR" = "YEAR")) %>% 
-  rename(POLITY2_EXT = POLITY2, 
+  rename(POLITY_EXT = POLITY, 
+         POLITY2_EXT = POLITY2, 
          TOTLIB1_EXT = TOTLIB1, 
          GNI_EXT = GNI,
          GNI_CAP_EXT = GNI_CAP, 
@@ -123,13 +150,13 @@ newdata <- masterset %>%
 
 #create odag data
 odag_data <- masterset %>% rename(ODAG251 = odag251) %>% 
-  select(CCODE_INT = CCODE, COUNTRY_INT = COUNTRY, YEAR, ODAG201:ODAG643) %>% 
-  mutate(across(ODAG201:ODAG643, as.numeric)) %>% 
-  pivot_longer(cols = ODAG201:ODAG643, names_to ="CCODE_EXT", values_to = "ODAG") %>% 
+  select(CCODE_INT = CCODE, COUNTRY_INT = COUNTRY, YEAR, ODAG201:ODAG643, ODAG251) %>% 
+  mutate(across(c(ODAG201:ODAG643,ODAG251), as.numeric)) %>% 
+  pivot_longer(cols = c(ODAG201:ODAG643,ODAG251), names_to ="CCODE_EXT", values_to = "ODAG") %>% 
   mutate(CCODE_EXT = gsub( "ODAG","",CCODE_EXT)) %>%
   mutate(CCODE_EXT = paste0("C", CCODE_EXT),
          CCODE_INT = paste0("C", CCODE_INT)) %>% 
-  filter(CCODE_INT != "CNA")
+  filter(CCODE_INT != "CNA" & ODAG != -99) 
 
 #create trade exports 
 trdex_data <- masterset %>% 
@@ -138,7 +165,19 @@ trdex_data <- masterset %>%
          YEAR, 
          matches("^TRDEX\\d{3}")) %>% 
   mutate(across(TRDEX351:TRDEX203, as.numeric)) %>% 
-  pivot_longer(cols = TRDEX351:TRDEX203, names_to ="CCODE_EXT", values_to = "TRDEX") %>% 
+  mutate(TRDEX200 = ifelse((is.na(TRDEX200) &
+                              is.na(TRDEX200h) &
+                              is.na(TRDEX200h)),
+                           NA,
+                           rowSums(across(c(TRDEX200, TRDEX200h, TRDEX200m)), na.rm = TRUE)
+  )) %>%
+  mutate(TRDEX321 = ifelse((is.na(TRDEX321sm) &
+                              is.na(TRDEX321sr) &
+                              is.na(TRDEX321yr)),
+                           NA,
+                           rowSums(across(c(TRDEX321sm, TRDEX321sr, TRDEX321yr)), na.rm = TRUE)
+  )) %>% #combining 321 yr, sm, se
+  pivot_longer(cols = TRDEX351:TRDEX321, names_to ="CCODE_EXT", values_to = "TRDEX") %>% 
   mutate(CCODE_EXT = gsub( "TRDEX","",CCODE_EXT)) %>% 
   mutate(CCODE_EXT = paste0("C", CCODE_EXT),
          CCODE_INT = paste0("C", CCODE_INT)) %>% 
@@ -149,13 +188,30 @@ trdim_data <- masterset %>%
   select(CCODE_INT = CCODE, 
          COUNTRY_INT = COUNTRY, 
          YEAR, 
-         matches("^TRDIM\\d{3}$")) %>% 
+         matches("^TRDIM\\d{3}")) %>% 
   mutate(across(TRDIM351:TRDIM203, as.numeric)) %>% 
-  pivot_longer(cols = TRDIM351:TRDIM203, names_to ="CCODE_EXT", values_to = "TRDIM") %>% 
+  mutate(TRDIM200 = ifelse((is.na(TRDIM200) &
+                              is.na(TRDIM200h) &
+                              is.na(TRDIM200h)),
+                           NA,
+                           rowSums(across(c(TRDIM200, TRDIM200h, TRDIM200m)), na.rm = TRUE)
+  )) %>% #summing 200, 200m, 200h to combine China
+  mutate(TRDIM321 = ifelse((is.na(TRDIM321sm) &
+                              is.na(TRDIM321sr) &
+                              is.na(TRDIM321yr)),
+                           NA,
+                           rowSums(across(c(TRDIM321sm, TRDIM321sr, TRDIM321yr)), na.rm = TRUE)
+  )) %>% #combining 321 yr, sm, se
+  mutate(TRDIM316 = ifelse((is.na(TRDIM316) &
+                              is.na(TRDIM316su)),
+                           NA,
+                           rowSums(across(c(TRDIM316,TRDIM316su)), na.rm = TRUE)
+  )) %>% #combining 316 and 316su (i.e. Russia and Soviet Union)
+  pivot_longer(cols = TRDIM351:TRDIM321, names_to ="CCODE_EXT", values_to = "TRDIM") %>% 
   mutate(CCODE_EXT = gsub( "TRDIM","",CCODE_EXT)) %>%
   mutate(CCODE_EXT = paste0("C", CCODE_EXT),
          CCODE_INT = paste0("C", CCODE_INT)) %>% 
-  filter(CCODE_INT != "CNA")
+  filter(CCODE_INT != "CNA") %>% filter(!(CCODE_INT == "C114" & CCODE_EXT == "C148" & YEAR == 1972))
   
 #merge on odag, trdex, trdim
 cleandata <- newdata %>% 
@@ -177,34 +233,50 @@ cleandata <- newdata %>%
   mutate(CONNECTIONlag1 = lag(CONNECTION))
 
 
-cleandata %>% view()
-
-
 #Data Viz
-ggplot(aes(y = logODAG), data = cleandata) + geom_boxplot()
-ggplot(aes(y = logTRDEX), data = cleandata) + geom_boxplot()
-ggplot(aes(y = logTRDIM), data = cleandata) + geom_boxplot()
-ggplot(aes(y = logGNI_EXT), data = cleandata) + geom_boxplot()
-ggplot(aes(y = logPOP_EXT), data = cleandata) + geom_boxplot()
+# ggplot(aes(y = logODAG), data = cleandata) + geom_boxplot()
+# ggplot(aes(y = logTRDEX), data = cleandata) + geom_boxplot()
+# ggplot(aes(y = logTRDIM), data = cleandata) + geom_boxplot()
+# ggplot(aes(y = logGNI_EXT), data = cleandata) + geom_boxplot()
+# ggplot(aes(y = logPOP_EXT), data = cleandata) + geom_boxplot()
+# cleandata_scaled <- cleandata %>%  mutate(across(TOTLIB1_EXT:logTRDEX, scale))
 
-cleandata_scaled <- cleandata %>%  mutate(across(TOTLIB1_EXT:logTRDEX, scale))
+#Add lags 
+
+# cleandata <- cleandata %>% arrange(COUNTRY_INT,COUNTRY_EXT, YEAR) %>% group_by(COUNTRY_INT, COUNTRY_EXT)  %>% 
+#   mutate(TOTLIB1_EXT_lag1 = lag(TOTLIB1_EXT,1),
+#          TOTLIB1_EXT_lag2 = lag(TOTLIB1_EXT,2),
+#          TOTLIB1_EXT_lag3 = lag(TOTLIB1_EXT,3),
+#          POLITY_EXT_lag1 = lag(POLITY_EXT,1),
+#          POLITY_EXT_lag2 = lag(POLITY_EXT,2),
+#          POLITY_EXT_lag3 = lag(POLITY_EXT,3),
+#          POLITY2_EXT_lag1 = lag(POLITY2_EXT,1),
+#          POLITY2_EXT_lag2 = lag(POLITY2_EXT,2),
+#          POLITY2_EXT_lag3 = lag(POLITY2_EXT,3)
+#   ) %>%  ungroup()
+
 
 library(lme4)
-mod0_TOTLIB1 <- glmer(CONNECTION ~ TOTLIB1_EXT  +  (1|COUNTRY_INT) + (1|COUNTRY_EXT)  + (1|YEAR), family = "binomial", data = cleandata_scaled, control = glmerControl(optimizer = "bobyqa"))
-mod1_TOTLIB1 <- glmer(CONNECTION ~ TOTLIB1_EXT  +  logGNI_EXT + logPOP_EXT + logODAG + logTRDEX + logTRDIM +  (1|COUNTRY_INT) + (1|COUNTRY_EXT) + (1|YEAR), family = "binomial", data = cleandata_scaled, control = glmerControl(optimizer = "bobyqa"))
-mod2_TOTLIB1 <- glmer(CONNECTION ~ TOTLIB1_EXT  + logGNI_EXT + logPOP_EXT + logODAG + logTRDEX + logTRDIM +  (1|COUNTRY_INT) + (1|COUNTRY_EXT)  + (1|YEAR), family = "binomial", data = cleandata_scaled, control = glmerControl(optimizer = "bobyqa"))
+#Base models 
+mod0_TOTLIB1 <- glmer(CONNECTION ~ TOTLIB1_EXT + (1|COUNTRY_INT) + (1|COUNTRY_EXT) +   (1|COUNTRY_INT:COUNTRY_EXT)   + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
+mod0_TOTLIB1_lag1 <- glmer(CONNECTION ~ TOTLIB1_EXT_lag1  +  (1|COUNTRY_INT) + (1|COUNTRY_EXT) + (1|COUNTRY_INT:COUNTRY_EXT)  + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
+mod0_TOTLIB1_lag2 <- glmer(CONNECTION ~ TOTLIB1_EXT_lag2  +  (1|COUNTRY_INT) + (1|COUNTRY_EXT) + (1|COUNTRY_INT:COUNTRY_EXT)  + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
+mod0_TOTLIB1_lag3 <- glmer(CONNECTION ~ TOTLIB1_EXT_lag3  +  (1|COUNTRY_INT) + (1|COUNTRY_EXT) + (1|COUNTRY_INT:COUNTRY_EXT)  + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
 
-mod0_POLITY <- glmer(CONNECTION ~ POLITY2_EXT  +  (1|COUNTRY_INT) + (1|COUNTRY_EXT)  + (1|YEAR), family = "binomial", data = cleandata_scaled, control = glmerControl(optimizer = "bobyqa"))
-mod1_POLITY <- glmer(CONNECTION ~ POLITY2_EXT  + logGNI_EXT + logPOP_EXT + logODAG + logTRDEX + logTRDIM +  (1|COUNTRY_INT) + (1|COUNTRY_EXT) + (1|YEAR), family = "binomial", data = cleandata_scaled, control = glmerControl(optimizer = "bobyqa"))
-mod2_POLITY <- glmer(CONNECTION ~ POLITY2_EXT  + logGNI_EXT + logPOP_EXT + logODAG + logTRDEX + logTRDIM +  (1|COUNTRY_INT) + (1|COUNTRY_EXT) + (1|YEAR), family = "binomial", data = cleandata_scaled, control = glmerControl(optimizer = "bobyqa"))
+mod0_POLITY <- glmer(CONNECTION ~ POLITY_EXT  +  (1|COUNTRY_INT) + (1|COUNTRY_EXT) +   (1|COUNTRY_INT:COUNTRY_EXT)  + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
+mod0_POLITY_lag1 <- glmer(CONNECTION ~ POLITY_EXT_lag1  + (1|COUNTRY_INT) + (1|COUNTRY_EXT) +   (1|COUNTRY_INT:COUNTRY_EXT)   + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
+mod0_POLITY_lag2 <- glmer(CONNECTION ~ POLITY_EXT_lag2  +  (1|COUNTRY_INT) + (1|COUNTRY_EXT) +   (1|COUNTRY_INT:COUNTRY_EXT)   + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
+mod0_POLITY_lag3 <- glmer(CONNECTION ~ POLITY_EXT_lag3  +  (1|COUNTRY_INT) + (1|COUNTRY_EXT) + (1|COUNTRY_INT:COUNTRY_EXT)   + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
 
-summary(mod0_TOTLIB1)
-summary(mod1_TOTLIB1)
-summary(mod2_TOTLIB1)
+mod0_POLITY2 <- glmer(CONNECTION ~ POLITY2_EXT  +  (1|COUNTRY_INT) + (1|COUNTRY_EXT) +   (1|COUNTRY_INT:COUNTRY_EXT)  + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
+mod0_POLITY2_lag1 <- glmer(CONNECTION ~ POLITY2_EXT_lag1  + (1|COUNTRY_INT) + (1|COUNTRY_EXT) +   (1|COUNTRY_INT:COUNTRY_EXT)   + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
+mod0_POLITY2_lag2 <- glmer(CONNECTION ~ POLITY2_EXT_lag2  +  (1|COUNTRY_INT) + (1|COUNTRY_EXT) +   (1|COUNTRY_INT:COUNTRY_EXT)   + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
+mod0_POLITY2_lag3 <- glmer(CONNECTION ~ POLITY2_EXT_lag3  +  (1|COUNTRY_INT) + (1|COUNTRY_EXT) + (1|COUNTRY_INT:COUNTRY_EXT)   + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
 
-summary(mod0_POLITY)
-summary(mod1_POLITY)
-summary(mod2_POLITY)
+#Full models 
+mod0_TOTLIB1 <- glmer(CONNECTION ~ TOTLIB1_EXT + logGNI_EXT + logPOP_EXT + logODAG + logTRDEX + logTRDIM+ (1|COUNTRY_INT) + (1|COUNTRY_EXT) +   (1|COUNTRY_INT:COUNTRY_EXT)   + (1|YEAR), family = "binomial", data = cleandata, control = glmerControl(optimizer = "bobyqa"))
+
+
 
 
 
